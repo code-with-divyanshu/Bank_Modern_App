c4e370952f99645f392f5ba82c48a7454af0d00c
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-10-02 12:07:39 +0530

    Create Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./styles";
+import { Navbar } from "./components";
 
-const App = () => {
-  return <div className="">App</div>;
-};
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}></div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}></div>
+    </div>
+  </div>
+);
 
 export default App;
